a394e16719a24a48ca819d72b099318d5d1e1ed3
Marcelo Oggero <marcelooggero@gmail.com>
2020-09-05 13:47:44 -0300

    Fin 3

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="100tifico-spa-2/">
             100tifi.co
           </a>
         </h1>
